394f541ef143feb90cf4555823bbb6c1903e6aab
pharmaexport <arkopharmaexport@gmail.com>
2026-06-29 08:38:33 +0200

    Rename home entry labels

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import { SiteNav } from "./components/SiteNav";
 export default function HomePage() {
   return (
     <main>
-      <SiteNav section="home" showSubTabs />
+      <SiteNav section="home" />
       <section className="hero pageSection" id="synthese">
         <div className="heroCopy">
           <p className="eyebrow">Synthèse</p>
           <h1>NutriAtlas</h1>
           <div className="ctaRow" id="acces">
-            <a className="primaryCta" href="/base">Accès base</a>
-            <a className="secondaryCta" href="/longevite">Longévité</a>
-            <a className="secondaryCta" href="/reco">Reco</a>
+            <a className="primaryCta" href="/base">Base aliments</a>
+            <a className="secondaryCta" href="/longevite">Diagnostic longévité</a>
+            <a className="secondaryCta" href="/reco">Reco âge bio</a>
           </div>
         </div>
       </section>
